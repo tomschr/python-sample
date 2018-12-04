7763839404e7cabbc94bb6a8b1c679001a392094
Thomas Schraitle <toms@suse.de>
2018-12-04 09:11:21 +0100

    Step 02: simple script with dependencies
    * Add requirements.txt

---
+++ 02/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(
+    # -- Project information
+    name='tuxfish',
+    version='0.1.0',
+    author='Tux Penguin',
+    author_email='tux (AT) example.org',
+    url='https://github.com/tomschr/python-project',
+    license='GPL-3.0',
+    keywords='example, Tux, fish',
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+      'Development Status :: 5 - Production/Stable'
+      #
+      'Intended Audience :: Developers',
+      # The license:
+      'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+      # Supported Python versions:
+      'Programming Language :: Python :: 3.4',
+      'Programming Language :: Python :: 3.5',
+      'Programming Language :: Python :: 3.6',
+    ],
+
+    # -- Description:
+    description='Makes a lot of fish',
+    long_description='...',
+
+    # Requirements:
+    install_requires=['requests'],
+
+    # -- Script(s):
+    scripts=['bin/tuxfish.py'],
+    )
